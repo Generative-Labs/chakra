3095a98ea44554b19ee3783ea1d4e2e253e10e65
jerrybaoo <61228234+jerrybaoo@users.noreply.github.com>
2024-03-08 18:12:47 +0800

    btc lock and redeem example

---
+++ btcserver/internal/btc/client.go
@@ -0,0 +1,34 @@
+package btc
+
+import "github.com/btcsuite/btcd/rpcclient"
+
+type Client struct {
+	rpcClient *rpcclient.Client
+}
+
+func New(config Config) (*Client, error) {
+	rpcClient, err := rpcclient.New(&rpcclient.ConnConfig{
+		Host:         config.RPCHost,
+		User:         config.RPCUser,
+		Pass:         config.RPCPass,
+		DisableTLS:   config.DisableTLS,
+		HTTPPostMode: true,
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := Client{
+		rpcClient: rpcClient,
+	}
+
+	return &client, nil
+}
+
+func (c *Client) CheckTransactionSignature(_, _, _ string) {}
+
+func (c *Client) CheckSignature() {}
+
+func (c *Client) CheckTransactionInclusion() {}
+
+func (c *Client) CheckTransactionFinalized() {}
